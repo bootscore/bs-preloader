aef497c915b8ace3bd708dd00bf2c292549efde6
craftwerk <pst@crftwrk.de>
2021-09-23 18:47:51 +0200

    Prevent fade-in if ctrl or cmd + click

--- js/preloader.js
+++ js/preloader.js
@@ -10,10 +10,15 @@ jQuery(document).ready(function ($) {
         $('#preloader').delay(350).fadeOut('slow');
     }, 1500);
 
-    // Preloader fade in on click <a>...</a> for smooth page transition
+    // Preloader fade-in on click <a>...</a> for smooth page transition
     $('a:not([href^=\\#]):not([target="_blank"]):not([href^="mailto:"]):not([href="javascript:void(0)"]):not(.add_to_cart_button):not(.blocks-gallery-item a):not(.dropdown-toggle):not([href="javascript:gaOptout()"]):not(.woocommerce-MyAccount-downloads-file):not(.entry-meta .comments-link a):not(.woocommerce-MyAccount-downloads-file):not([data-fade="false"])').click(function () {
-        $("#preloader").fadeIn('fast');
-        $('#preloader').delay(1000).fadeOut('slow');
-    });    
+        if (event.ctrlKey || event.metaKey) {
+            //is ctrl or cmd + click do nothing
+        } else {
+            // preloader fade-in
+            $("#preloader").fadeIn('fast');
+            $('#preloader').delay(1000).fadeOut('slow');
+        }
+    });
 
 });
