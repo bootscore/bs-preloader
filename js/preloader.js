54e5930c828e1997779d90fa166cc3613ea2096e
craftwerk <pst@crftwrk.de>
2021-11-07 10:44:16 +0100

    Reformat files

--- js/preloader.js
+++ js/preloader.js
@@ -1,17 +1,17 @@
 jQuery(document).ready(function ($) {
-    
-    // Preloader
-    $(window).bind('beforeunload',function(){
-        $("#preloader").fadeIn('fast');
-        $('#status').fadeIn('fast');
-    })
-    $(window).on('load', function () {
-        $('#status').fadeOut();
-        $('#preloader').delay(350).fadeOut('slow');
-    })
-    setTimeout(function () {
-        $('#status').fadeOut();
-        $('#preloader').delay(350).fadeOut('slow');
-    }, 1500);
+
+  // Preloader
+  $(window).bind('beforeunload', function () {
+    $("#preloader").fadeIn('fast');
+    $('#status').fadeIn('fast');
+  })
+  $(window).on('load', function () {
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+  })
+  setTimeout(function () {
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+  }, 1500);
 
 });
--- templates/preloader.php
+++ templates/preloader.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Don't allow direct access
 ?>
 
 <div id="preloader" class="align-items-center justify-content-center position-fixed top-0 end-0 bottom-0 start-0 zi-1070">
-    <div id="status" class="spinner-border text-primary" role="status">
-        <span class="sr-only">Loading...</span>
-    </div>
+  <div id="status" class="spinner-border text-primary" role="status">
+    <span class="sr-only">Loading...</span>
+  </div>
 </div>
