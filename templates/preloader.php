7214a634343b677d752ebd8eb7c809950d09b637
craftwerk <pst@crftwrk.de>
2024-01-06 14:45:27 +0100

    Add class bg-body to #preloader

--- templates/preloader.php
+++ templates/preloader.php
@@ -3,11 +3,11 @@
 /**
  * Preloader template.
  *
- * This template can be overriden by copying this file to your-theme/bs-preloader-main/preloader.php
+ * This template can be overriden by copying this file to your-theme/bs-preloader/preloader.php
  *
  * @author   Bootscore
- * @package  bS Preloader
- * @version  5.1.1
+ * @package  bs Preloader
+ * @version  5.2.1
  */
 
 // Exit if accessed directly
@@ -15,7 +15,7 @@ defined( 'ABSPATH' ) || exit;
 
 ?>
 
-<div id="preloader" class="align-items-center justify-content-center position-fixed top-0 end-0 bottom-0 start-0 zi-1070">
+<div id="preloader" class="bg-body align-items-center justify-content-center position-fixed top-0 end-0 bottom-0 start-0 zi-1070">
   <div id="status" class="spinner-border text-primary" role="status">
     <span class="visually-hidden">Loading...</span>
   </div>
