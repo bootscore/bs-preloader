b8fd6131185064430089e438828066517ea51642
craftwerk <pst@crftwrk.de>
2024-10-02 11:03:40 +0200

    Added a hook inside #status

--- templates/preloader.php
+++ templates/preloader.php
@@ -7,7 +7,7 @@
  *
  * @author   Bootscore
  * @package  bs Preloader
- * @version  5.4.0
+ * @version  5.5.0
  */
 
 // Exit if accessed directly
@@ -17,6 +17,7 @@ defined( 'ABSPATH' ) || exit;
 
 <div id="preloader" class="<?= apply_filters('bootscore/class/preloader_bg', 'bg-body align-items-center justify-content-center position-fixed top-0 end-0 bottom-0 start-0 zi-1070'); ?>">
   <div id="status" class="<?= apply_filters('bootscore/class/preloader_spinner', 'spinner-border text-primary'); ?>" role="status">
+    <?php do_action( 'bootscore_preloader_after_status_open' ); ?>
     <span class="visually-hidden">Loading...</span>
   </div>
 </div>
