aa17d6bc210da3a4aeacf0c26f2374ef7c8f24a5
craftwerk <pst@crftwrk.de>
2025-04-16 19:05:50 +0200

    Added a timestamp to prevent caching if CSS/JS files have been changed

--- main.php
+++ main.php
@@ -36,11 +36,19 @@ $myUpdateChecker->setBranch('main');
  * Register Styles and Scripts
  */
 function bs_preloader_scripts() {
+  // File paths
+  $script_file = plugin_dir_path(__FILE__) . 'assets/js/preloader.js';
+  $style_file  = plugin_dir_path(__FILE__) . 'assets/css/preloader.css';
 
-  wp_enqueue_script('preloader-js', plugins_url('/assets/js/preloader.js', __FILE__), array('jquery'), '1.0', true);
+  // Timestamps as version numbers
+  $script_ver = file_exists($script_file) ? date('YmdHi', filemtime($script_file)) : false;
+  $style_ver  = file_exists($style_file) ? date('YmdHi', filemtime($style_file)) : false;
 
-  wp_register_style('preloader-css', plugins_url('/assets/css/preloader.css', __FILE__));
-  wp_enqueue_style('preloader-css');
+  // Enqueue script
+  wp_enqueue_script('preloader-js', plugins_url('/assets/js/preloader.js', __FILE__), ['jquery'], $script_ver, true);
+
+  // Enqueue style
+  wp_enqueue_style('preloader-css', plugins_url('/assets/css/preloader.css', __FILE__), [], $style_ver);
 }
 
 add_action('wp_enqueue_scripts', 'bs_preloader_scripts');
