83c124a939bf7e49ebecaab771b2d6fec43bf27f
craftwerk <pst@crftwrk.de>
2025-12-22 14:43:59 +0100

    Escape HTML in filters

--- templates/preloader.php
+++ templates/preloader.php
@@ -7,7 +7,7 @@
  *
  * @author   Bootscore
  * @package  bs Preloader
- * @version  5.5.0
+ * @version  5.5.2
  */
 
 // Exit if accessed directly
@@ -15,8 +15,8 @@ defined( 'ABSPATH' ) || exit;
 
 ?>
 
-<div id="preloader" class="<?= apply_filters('bootscore/class/preloader/bg', 'bg-body align-items-center justify-content-center position-fixed top-0 end-0 bottom-0 start-0 zi-1070'); ?>">
-  <div id="status" class="<?= apply_filters('bootscore/class/preloader/spinner', 'spinner-border text-primary'); ?>" role="status">
+<div id="preloader" class="<?= esc_attr( apply_filters('bootscore/class/preloader/bg', 'bg-body align-items-center justify-content-center position-fixed top-0 end-0 bottom-0 start-0 zi-1070') ); ?>">
+  <div id="status" class="<?= esc_attr( apply_filters('bootscore/class/preloader/spinner', 'spinner-border text-primary') ); ?>" role="status">
     <?php do_action( 'bootscore_preloader_status' ); ?>
     <span class="visually-hidden">Loading...</span>
   </div>
